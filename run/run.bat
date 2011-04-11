e1db539eeb3670701889b2145312b5e5bf3791df
eyalhoc <eyalhoc@16029bd5-f51d-4246-a912-480e4ba64f9e>
2011-04-11 15:44:37 +0000

    added dos batch file for windows

---
+++ run/run.bat
@@ -0,0 +1,6 @@
+
+echo off
+
+..\..\..\robust.exe ../src/base/axi2apb.v -od out -I ../src/gen -list apblist.txt -listpath -header
+
+echo Completed RobustVerilog axi2apb run - results in run/out/
--- run/run.sh
+++ run/run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo Starting RobustVerilog axi2apb run
-rm -rf out
-mkdir out
-
 ../../../robust ../src/base/axi2apb.v -od out -I ../src/gen -list apblist.txt -listpath -header
 
 echo Completed RobustVerilog axi2apb run - results in run/out/
