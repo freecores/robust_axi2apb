a4cea23cb973ae8a16f4df9d406e762acc338800
eyalhoc <eyalhoc@16029bd5-f51d-4246-a912-480e4ba64f9e>
2011-04-29 15:43:56 +0000

    fixed run scripts

--- run/run.bat
+++ run/run.bat
@@ -1,6 +1,6 @@
 
 echo off
 
-..\..\..\robust.exe ../src/base/axi2apb.v -od out -I ../src/gen -list apblist.txt -listpath -header
+..\..\..\robust.exe ../src/base/axi2apb.v -od out -I ../src/gen -list list.txt -listpath -header
 
 echo Completed RobustVerilog axi2apb run - results in run/out/
--- run/run.sh
+++ run/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-../../../robust ../src/base/axi2apb.v -od out -I ../src/gen -list apblist.txt -listpath -header
+../../../robust ../src/base/axi2apb.v -od out -I ../src/gen -list list.txt -listpath -header ${@}
 
 echo Completed RobustVerilog axi2apb run - results in run/out/
